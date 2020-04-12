17427e76004704af4ce28560e36b9c4adf131d9c
lenaz <lena.zelenaya@gmail.com>
2020-04-12 15:22:48 +0300

    "Hello World!" with Spring boot

---
+++ mvc_demo/src/main/java/mvc/com/mvc_demo/MvcDemoApplication.java
@@ -0,0 +1,13 @@
+package mvc.com.mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcDemoApplication.class, args);
+    }
+
+}
