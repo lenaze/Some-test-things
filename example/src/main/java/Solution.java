a78be770ae389eaf68a98f879427d6ac6a819f96
lenaz <lena.zelenaya@gmail.com>
2020-04-12 11:17:33 +0300

    Example of maven project

---
+++ example/src/main/java/Solution.java
@@ -0,0 +1,9 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class Solution {
+    public static void main(String[] args) {
+        File tempDirectory = FileUtils.getTempDirectory();
+    }
+}
