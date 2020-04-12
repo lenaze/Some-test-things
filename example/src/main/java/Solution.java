643fb5a4e8512f328924c35a2fc2fb4280dba89e
lenaz <lena.zelenaya@gmail.com>
2020-04-12 12:17:43 +0300

    Servlet + Maven. List a sum of visitors.

--- example/src/main/java/Solution.java
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-
-public class Solution {
-    public static void main(String[] args) {
-        File tempDirectory = FileUtils.getTempDirectory();
-    }
-}
