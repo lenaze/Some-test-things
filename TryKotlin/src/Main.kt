f4990ec49a4386055a95ab36d770f2ffcbd69993
lenaz <lena.zelenaya@gmail.com>
2020-04-16 09:11:36 +0300

    Some function. NullPointerException.

--- TryKotlin/src/Main.kt
+++ TryKotlin/src/Main.kt
@@ -1,7 +1,29 @@
 fun main(args: Array<String>): Unit {
-    var UserJava = UserJava("It's a var from Java!");
+    val UserJava = UserJava("It's a var from Java!");
     println("Example java: ${UserJava.name}")
 
-    var userKotlin = UserKotlin("It's a var from Kotlin!")
+    val userKotlin = UserKotlin("It's a var from Kotlin!")
     println("Example Kotlin: ${userKotlin.name}")
+
+    val a0: Int = 123
+    val a1: Long = 123
+    println(myFirstFunction(a0, a1))
+
+    var a2: Boolean = true
+    val a3: Char = 'h'
+    println(mySecondFunction(a3))
+
+    val str: String?
+    str = myThirdFunction()
+    var size = str!!.length //kotlin.KotlinNullPointerException
+}
+
+fun myFirstFunction(a: Int, b: Long): Long{
+    return a * b
+}
+
+fun mySecondFunction(char: Char) = char == 'h'
+
+fun myThirdFunction(): String? {
+    return null
 }
