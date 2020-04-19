e32a1d644154796b9582b9b407ca991dcb3a52cd
lenaz <lena.zelenaya@gmail.com>
2020-04-19 12:02:17 +0300

    First steps in making blog with Spring boot

---
+++ blog/src/main/java/com/zelly/blog/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.zelly.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home page");
+        return "home";
+    }
+
+    @GetMapping("/blog-main")
+    public String blog(Model model) {
+        model.addAttribute("title", "Home page");
+        return "blog-main";
+    }
+}
